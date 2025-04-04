b958437904f4170c6f2f01a3ed5f037bb48ad7df
Arthur <a.arthuraraujo1711@gmail.com>
2025-04-04 11:56:55 -0300

    Criando projeto

---
+++ src/js/script.js
@@ -0,0 +1,110 @@
+// DECLARAÇÃO E VARIAVEIS
+
+var nome ="Fiap";
+console.log(nome)
+
+let idade ="19"
+console.log(idade)
+
+const sobrenome ="Fiapinho"
+console.log(sobrenome)
+
+// undefined
+
+let exemplo;
+console.log(exemplo)
+
+// null
+
+let exemplo1 =null
+console.log(exemplo1)
+
+// TIPOS DE VARIAVEIS
+
+let ex1 ="Fiap";
+console.log(typeof ex1)
+
+let ex2 =25;
+console.log(typeof ex2)
+
+let ex3 =true;
+console.log(typeof ex3)
+
+let ex4={};
+console.log(typeof ex4)
+
+let ex5=[];
+console.log(typeof ex5)
+
+//Conversões
+
+//FLOAT => STRING
+
+let numFloat = 123.456
+console.log(numFloat.toString())
+
+//STRING => FLOAT
+let numString ="12.963";
+console.log(parseFloat(numString))
+
+//INT => STRING
+let numINT ="20.87";
+console.log(numINT.toString())
+
+//STRING => INT
+let numString2 ="120";
+console.log(parseInt(numString2))
+
+//METODOS
+
+//METODO LENGTH - VERIFICA O TAMANHO DA STRING
+
+let frase="O mundo da tecnologia";
+console.log(frase.length)
+
+//MEtoDO indexof - RETORNA UM TRECHO DO SEU CÒDIGO
+
+let texto ="Programação sustentável";
+console.log(texto.lastIndexOf('ão'))
+
+//METODO SLICE - RETORNa PARTE DE UM TEXTO PASSANDO INICIO E O FINAL
+let info ="Programação de ponta";
+console.log(info.slice(14,20))
+
+//OPERADORES ARITIMÉTICOS
+
+const a=10
+const b=20
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+//OPERADORES LÓGICOS
+
+const c=20;
+const d=30;
+
+console.log(c > d);
+console.log(c > d && c< 10);
+console.log(c > d || d > c);
+console.log(c == d || d <= c);
+
+//OPERADORES DE COMPARAÇÃO
+
+const e=10
+const f=30
+
+// console.log(e == f);
+// console.log(e === f);
+// console.log(e != f);
+
+// CONDICIONAL
+
+
+
+
+
+
+
